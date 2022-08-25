4c262f448fae748b1134f5daa08579650db33bae
MateusFCD <mateusdrumondicm@gmail.com>
2022-08-25 16:17:19 -0300

    update

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 import DataContext from '../../data/DataContext'
+import { AppContext } from '../../data/Store'
 
 const UseContext = (props) => {
 
@@ -13,6 +14,14 @@ const UseContext = (props) => {
             number: state.number + delta
         })
     }
+
+    const {number, text, setNumber, setText} = useContext(AppContext)
+
+    useEffect(function() {
+        if(number > 1250) {
+            setText('Eita')
+        }
+    }, [number])
 
     return (
         <div className="UseContext">
@@ -29,6 +38,16 @@ const UseContext = (props) => {
                 <div>
                     <button className="btn" onClick={() => addNumber(-1)}>-1</button>
                     <button className="btn" onClick={() => addNumber(+1)}>+1</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercício #02"/>
+            <div className="center">
+                <span className="text">{text}</span>
+                <span className="text">{number}</span>
+                <div>
+                    <button className="btn" onClick={() => setNumber(number -1)}>-1</button>
+                    <button className="btn" onClick={() => setNumber(number +1)}>+1</button>
                 </div>
             </div>
         </div>
